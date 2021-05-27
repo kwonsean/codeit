10063098b3456c750ca9e7955f3b151e94cae548
ksh <gownn12@naver.com>
2021-05-27 22:50:12 +0900

    Add get_sum_ver1 function from test

--- calculator.py
+++ calculator.py
@@ -1,27 +1,42 @@
 # 기본 계산기
 def add(a, b):
-  return a+b
+    return a+b
+
 
 def subtract(a, b):
-  return a-b
+    return a-b
+
 
 def multiply(a, b):
-  return a * b
+    return a * b
+
 
 def divide(a, b):
-  return a / b
+    return a / b
 
-def get_Median(a,b):
-  return (a + b)/2
- 
+
+def get_Median(a, b):
+    return (a + b)/2
+
+
+def get_Median(a, b):
+    return (a + b)/2
+
+
 def get_Remainder(a, b):
-  return a//b
+    return a//b
+
 
 def get_Abs(num):
-  if num >= 0:
-    return num
-  else:
-    return -num
-    
-def get_Percet(a,b):
-  return ( a/ b) * 100
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+
+def get_Percet(a, b):
+    return (a / b) * 100
+
+
+def get_Sum_ver1(n):
+    return n(n+1)/2
